e3c075748af8fff349f41f3e1e2115123c1497e6
wtzmuda <wtzmuda@gmail.com>
2025-10-18 17:44:35 +0200

    Zejściówka 1 v1

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,4 +1,30 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
 class Wektor2D
 {
-    // Tutaj napisz implementacje klasy Wektor2D
+    
+private:
+    double x, y;
+
+public:
+    Wektor2D() : x(0), y(0) {}
+
+    void   setX(double X0) { x = X0; }
+    double getX() const { return x; }
+
+    void   setY(double Y0) { y = Y0; }
+    double getY() const { return y; }
+
+    Wektor2D(double X0, double Y0) : x(X0), y(Y0) {}
+
+    Wektor2D operator+(const Wektor2D& other) const {
+        return Wektor2D(x + other.x, y + other.y);
+    }
+
+    double operator*(const Wektor2D& other) const {
+        return x * other.x + y * other.y;
+    }
 };
